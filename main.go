20d218f0d4cf661bae851ef508d9da3acd3e9a33
Adam <adammmmm@users.noreply.github.com>
2026-01-01 13:43:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/scottdware/go-junos"
+	"github.com/adammmmm/go-junos"
 	"go.uber.org/zap"
 )
 
